91bc6c7a2803234f0bf09d5cf0d66182f952f9f0
ramos-04 <65966722+ramos-04@users.noreply.github.com>
2020-06-07 14:23:52 +0530

    Update docker-install.sh

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -2,14 +2,13 @@
 
 # This script is used to install docker on Linux ubuntu 
 
-# If any of the commands returns a non-zero(error) status, then it could be caught and the script execution can be terminated immediately
+# If any command returns a non-zero(error) status, then it could be caught and the script execution can be terminated immediately
 set -e
 
 # used to catch errors
 trap 'catcherror $? $LINENO' ERR
 
-# The below function acts like a catch block used in exception handling which is observed in different programming languages. It will print the error code and the line number on which the error was generated. The script will be exited later.
-
+# The below function acts like a catch block used in exception handling(observed in different programming languages). It will print the error code and the line number on which the error was generated. The script will be exited later
 catcherror()
 {
   echo "Error code $1 generated on Line $2"
@@ -31,13 +30,13 @@ apt update -y
 # installing some prerequisites which let 'apt' use packages over HTTPS
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-# adding the GPG key for the official Docker repository to our system
+# adding the GPG key for the official docker repository to our system
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-# adding the Docker repository to APT sources
+# adding the docker repository to APT sources
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
-# updating the package database with the Docker packages from the newly added repo
+# updating the package database with the docker packages from the newly added repo
 apt update -y
 
 # installing docker
