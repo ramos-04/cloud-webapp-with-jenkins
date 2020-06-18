a7004a54f18f35808f84aaa3ae95847c71f5c3fd
ramos-04 <ashish.ramgude2018@gmail.com>
2020-06-18 17:44:04 +0530

    build docker image to form an artifact which will be pushed to S3

--- scripts/codedeploy/dependencies.sh
+++ scripts/codedeploy/dependencies.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-IMAGE_NAME=python-django-server-img
+#IMAGE_NAME=python-django-server-img
 FILE_NAME=python-django-server-img.tar
-GITHUB_URL=https://github.com/ramos-04/cloud-project.git
+#GITHUB_URL=https://github.com/ramos-04/cloud-project.git
 
-apt-get update
+#apt-get update
 
-apt-get install -y git
+#apt-get install -y git
 
 # cloning the repository
-git clone $GITHUB_URL
+#git clone $GITHUB_URL
 
-cd cloud-project/webserver
+#cd cloud-project/webserver
 
 # building the python django docker image
-echo "building the python docker image"
-docker build -t $IMAGE_NAME .
+#echo "building the python docker image"
+#docker build -t $IMAGE_NAME .
+
+echo "loading the docker image from a tar file"
+docker load --input $FILE_NAME
 
 # listing docker images
 echo "listing docker images: "
 docker images
-
-
-#echo "loading the docker image from a tar file"
-#docker load --input $FILE_NAME
-
-
