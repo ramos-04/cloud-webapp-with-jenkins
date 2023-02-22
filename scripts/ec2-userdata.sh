3af470ac8e6c1b17da043af7dff51b4657cd46fe
ashish <ashish.ramgude2018@gmail.com>
2023-02-22 15:12:59 +0530

    support for pipefail in the userdata script

--- scripts/ec2-userdata.sh
+++ scripts/ec2-userdata.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # This script tries to leverage the 'userdata' functionality of the EC2 service in AWS cloud. When the EC2 instance will be launched, this script will run during the boot time, thus, ultimately installing Docker and a CodeDeploy agent.
 
-# If any command returns a non-zero(error) status, then it could be caught and the script execution can be terminated immediately
-set -e
+# When you encounter an error in a bash script, by default, it throws an error message to stderr, but continues its execution in the rest of the script. In fact you see the same behavior in a terminal window; even if you type a wrong command by accident, it will not kill your terminal. You will just see the "command not found" error, but you terminal/bash session will still remain. If any command returns a non-zero(error) status, then it could be caught and the script execution can be terminated immediately by using the set -e command. However, one special error condition not captured by set -e is when an error occurs somewhere inside a pipeline of commands. This is because a pipeline returns a non-zero status only if the last command in the pipeline fails. Any error produced by previous command(s) in the pipeline is not visible outside the pipeline, and so does not kill a bash script. Thus, -o pipefail option is added.
 
-# used to catch errors
+set -o pipefail -e
+
+
+
+# A Bash trap allows you to execute a function when a signal is trapped. When any command inside the shellscript will return a non-zero status(error), the trap command will trap it and send to the fucntion. $? is a builtin shell variable which stores the exit status of the last run command.
+
 trap 'catcherror $? $LINENO' ERR
 
-# The below function acts like a catch block used in exception handling(observed in different programming languages). It will print the error code and the line number on which the error was generated. The script will be exited later
+# The below function acts like a catch block used in exception handling(observed in different programming languages). It will print the error code and the line number on which the error was generated. The script will be exited later. If you dont want to exit the script on a particular error, then you can manage the exit command inside catch block flexibly by creating conditions.
+
 catcherror()
 {
   echo "Error code $1 generated on Line $2"
