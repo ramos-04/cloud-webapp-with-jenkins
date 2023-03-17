dd9e44d4a4ede17e96e56cec835e54f1a1c599ae
ashish <ashish.ramgude2018@gmail.com>
2023-03-17 21:50:38 +0530

    integrated codedeploy with jenkins

--- scripts/jenkins-shell-environment.sh
+++ scripts/jenkins-shell-environment.sh
@@ -5,21 +5,23 @@ pwd
 
 ls
 
+echo "printing the docker version"
+
 docker version
 
 cd webserver
 
 echo "building the python docker image"
 
-docker build -t $IMAGE_NAME:$IMAGE_VERSION .
+docker build -t $LOCAL_IMAGE_NAME:$IMAGE_VERSION .
 
-echo "listing docker images: "
+echo "listing the new docker images: "
 
 docker images
 
 echo "tag your image so you can push the image to this repository"
 
-docker tag $IMAGE_NAME:$IMAGE_VERSION public.ecr.aws/t4y9j5k5/$IMAGE_NAME:$IMAGE_VERSION
+docker tag $LOCAL_IMAGE_NAME:$IMAGE_VERSION $ECR_REPOSITORY_NAME/$LOCAL_IMAGE_NAME:$IMAGE_VERSION
 
 echo "listing docker images to check the new tag "
 
@@ -27,8 +29,13 @@ docker images
 
 echo "Retrieve an authentication token and authenticate your Docker client to your registry."
 
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/t4y9j5k5
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPOSITORY_NAME
 
 echo "pushing the image to the registry"
 
-docker push public.ecr.aws/t4y9j5k5/$IMAGE_NAME:$IMAGE_VERSION
+docker push $ECR_REPOSITORY_NAME/$LOCAL_IMAGE_NAME:$IMAGE_VERSION
+
+
+
+
+
